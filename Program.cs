44352a40284637041a9cc61923b644065954d63c
MartinArvidsson <ma223ku@lnu.student.se>
2014-09-23 14:26:45 +0200

    fortsätter att arbeta, ska snart byta plats att arbeta på.

--- Program.cs
+++ Program.cs
@@ -10,12 +10,39 @@ namespace Godtycklig_lönerevison
     {
         static void Main(string[] args)
         {
-         int Paychecks;
-         Paychecks = ReadInt("Ange antal löner som ska bearbetas: ");
+            while (true)
+            {
+                try
+                {
+                    int Paychecks;
+                    Paychecks = ReadInt("Ange antal löner som ska bearbetas: ");
+                    if (Paychecks <= 1)                   
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Fel! antal löner måste vara fler än 1!");
+                        Console.ResetColor();
+                        return;
+                    }
+                    processSalaries(Paychecks);
+                    break;
+                }
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Fel! du har inte angivit ett heltal! Eller så är talet för stort!");
+                    Console.ResetColor();
+                }
+            }
         }
         static void processSalaries(int count)
         {
-            int[] SalariesArray = new int[];
+            int[] SalariesArray = new int[count];
+            
+            for (int i = 0; i < SalariesArray.Length; ++i)
+            {
+                SalariesArray[i] = ReadInt("Ange lön nummer " + (i+1) + ": ");
+                
+            }
         }
         static int ReadInt(string prompt)
         {
