6a448eb9c938e2b2a5de2a327e972823448822d5
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2014-09-23 22:07:14 +0200

    Klar med labb 1.3

--- Program.cs
+++ Program.cs
@@ -8,57 +8,48 @@ namespace Godtycklig_lönerevison
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Main
         {
-            do
+            do 
             {
                 while (true)
                 {
                     try
                     {
                         int Paychecks;
-                        Paychecks = ReadInt("Ange antal löner som ska bearbetas: ");
+                        Paychecks = ReadInt("Ange antal löner som ska bearbetas: "); //Läser in variablen som blir antalet löner
                         if (Paychecks <= 1)
                         {
                             Console.BackgroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Fel! antal löner måste vara fler än 1!");
+                            Console.WriteLine("Fel! antal löner måste vara fler än 1!"); //Felmeddelande som visas om variablen är <1
                             Console.ResetColor();
                             Console.WriteLine();
-                            Console.BackgroundColor = ConsoleColor.Green;
-                            Console.WriteLine("Tryck valfri tangent för ny beräkning, Esc avslutar");
-                            Console.ResetColor();
-                            if (Console.ReadKey(true).Key != ConsoleKey.Escape) ;
-                            {
-
-                            }
-                            while (Console.ReadKey(true).Key == ConsoleKey.Escape) ;
-                            {
-                                return;
-                            }
-
-
                         }
+                        else
                         processSalaries(Paychecks);
+                        Console.WriteLine();
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Tryck valfri tangent för ny beräkning, Esc avslutar"); //Startar om applikatonen om variablen var <1 eller när programmet var klart.
+                        Console.ResetColor();
                         break;
                     }
                     catch
                     {
                         Console.BackgroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Fel! du har inte angivit ett heltal! Eller så är talet för stort!");
+                        Console.WriteLine("Fel! du har inte angivit ett heltal! Eller så är talet för stort!"); //Felmeddelande om man har bokstäver eller ett tal över 1 miljard.
                         Console.ResetColor();
                     }
                 }
-
-            } while  (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape); //Alla knappar utom esc. startar om programmet.
         }
             
-        static void processSalaries(int count)
+        static void processSalaries(int count) // Här sker beräkningar och uträkningar
         {
             int[] SalariesArray = new int[count];
             
             for (int i = 0; i < SalariesArray.Length; ++i)
             {
-                SalariesArray[i] = ReadInt("Ange lön nummer " + (i+1) + ": ");
+                SalariesArray[i] = ReadInt("Ange lön nummer " + (i+1) + ": "); //Beräknar hur många "lådor" arrayen ska ha
                 
             }
             int[] PresentationSalaries = (int[])SalariesArray.Clone();
@@ -69,13 +60,13 @@ namespace Godtycklig_lönerevison
             {
                 int MedianNumber1 = SalariesArray.Length / 2 ;
                 int MedianNumber2 = SalariesArray.Length / 2 - 1;
-                int Median1 = (SalariesArray[MedianNumber1] + SalariesArray[MedianNumber2]) / 2;
+                int Median1 = (SalariesArray[MedianNumber1] + SalariesArray[MedianNumber2]) / 2; // Räknar ut medianen
                 Console.WriteLine("Medianlön :      {0:c0} ", Median1);
             }
             if (SalariesArray.Length % 2 == 1)
             {
                 int Median2 = SalariesArray.Length / 2;
-                Console.WriteLine("Medianlön :      {0:c0} ", SalariesArray[Median2]);
+                Console.WriteLine("Medianlön :      {0:c0} ", SalariesArray[Median2]); //Median
             }
             Console.WriteLine("Medellön :       {0:c0}", SalariesArray.Average()); //Medellön
             int Differerence = SalariesArray.Max() - SalariesArray.Min(); // Räknar differensen
@@ -87,14 +78,14 @@ namespace Godtycklig_lönerevison
                 {
                     Console.WriteLine();
                 }
-                Console.Write(" {0,6 }", PresentationSalaries[i]);
+                Console.Write(" {0,6 }", PresentationSalaries[i]); // Presenterar lönerna i ordningen
             }
         }
-        static int ReadInt(string prompt)
+        static int ReadInt(string prompt) //Här anges variablen salaries
         {
             int salaries;
             Console.Write(prompt);
-            salaries = int.Parse(Console.ReadLine());
+            salaries = int.Parse(Console.ReadLine()); 
             return salaries;
         }
     }
